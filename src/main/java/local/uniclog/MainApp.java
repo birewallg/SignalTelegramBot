c9d35b84727cdc85d459bb781ec7fc53df12857b
erlidione <g.sstrav@gmail.com>
2021-12-14 21:34:40 +0300

    Add bot inline method

--- src/main/java/local/uniclog/MainApp.java
+++ src/main/java/local/uniclog/MainApp.java
@@ -1,10 +1,35 @@
 package local.uniclog;
 
 import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
 public class MainApp {
+
     public static void main(String[] args) {
+        new MainApp().action();
+    }
+
+    public void action() {
         log.info("MainApp init");
     }
+
+    /* telegram bot section START */
+    public static TelegramBotCore telegramBotCore;
+
+    public void telegramBotInit() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(telegramBotCore = new TelegramBotCore());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msg) {
+        telegramBotCore.sendQuery(msg);
+    }
+    /* telegram bot section END */
 }
