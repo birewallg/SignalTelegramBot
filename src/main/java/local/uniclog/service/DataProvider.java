f876dbff2da1745112cf9f8a3fad7be5d11060d9
erlidione <g.sstrav@gmail.com>
2021-12-14 03:15:23 +0300

    DataProvider cast protected methods

--- src/main/java/local/uniclog/service/DataProvider.java
+++ src/main/java/local/uniclog/service/DataProvider.java
@@ -6,15 +6,15 @@ import java.lang.reflect.Type;
 
 public class DataProvider {
 
-    public <T>String save(T object) {
+    protected <T>String save(T object) {
         return new Gson().toJson(object);
     }
 
-    public <T>T load(String json, Class<T> list) {
+    protected <T>T load(String json, Class<T> list) {
         return new Gson().fromJson(json, list);
     }
 
-    public <T>T load(String json, Type type) {
+    protected <T>T load(String json, Type type) {
         return new Gson().fromJson(json, type);
     }
 }
